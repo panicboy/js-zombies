1b788d1bf453f32acfcfde9c03d647758db984ae
panicboy <asasaki@gmail.com>
2016-06-15 12:11:21 -1000

    tests passed: Item, Weapon, Food, Player #1

--- zombies.js
+++ zombies.js
@@ -8,6 +8,10 @@
  * @property {string} name
  */
 
+ function Item(name) {
+    this.name = name;
+  }
+
 
 /**
  * Class => Weapon(name, damage)
@@ -15,7 +19,7 @@
  * Creates a weapon item.
  * Weapon items can be equipped for use in battle.
  *
- * The Weapon class constructor will call 
+ * The Weapon class constructor will call
  *   the super class (Item) constructor
  *   while passing in the 1 Item constructor param
  *
@@ -25,12 +29,22 @@
  * @property {number} damage
  */
 
+ function Weapon(name, damage) {
+  this.damage = damage;
+  Item.call(this, name);
+}
+
+
 
 /**
  * Weapon Extends Item Class
  * -----------------------------
  */
-
+  Weapon.prototype = Object.create(Item.prototype, {
+    constructor: {
+      value: Item
+    }
+  });
 
 
 /**
@@ -39,7 +53,7 @@
  * Creates a food item.
  * Food items give energy, restoring health to the player.
  *
- * The Food class constructor will call 
+ * The Food class constructor will call
  *   the super class (Item) constructor
  *   while passing in the 1 Item constructor param
  *
@@ -49,12 +63,22 @@
  * @property {number} energy
  */
 
+function Food(name, energy) {
+  this.energy = energy;
+  Item.call(this, name);
+}
+
 
 /**
  * Food Extends Item Class
  * -----------------------------
  */
 
+Food.prototype = Object.create(Item.prototype, {
+    constructor: {
+      value: Item
+    }
+  });
 
 
 /**
@@ -78,6 +102,40 @@
  * @property {method} getPack              Returns private variable `pack`.
  * @property {method} getMaxHealth         Returns private variable `maxHealth`.
  */
+
+function Player(name, health, strength, speed){
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+  this.speed = speed;
+  var pack = [];
+  var maxHealth = health;
+
+  function _getPack(){
+    return pack;
+  }
+
+  function _getMaxHealth() {
+    return maxHealth;
+  }
+
+  function _takeItem(item) {
+    thePack = _getPack();
+    if(thePack.length >= 3) {
+      return false;
+    }
+
+  }
+
+//external methods
+return {
+  getPack: _getPack,
+  getMaxHealth: _getMaxHealth,
+  takeItem: _takeItem
+};
+
+//end of Player function
+}
 
 
 /**
@@ -231,7 +289,7 @@
  * -----------------------------
  * Creates a fast zombie.
  *
- * The FastZombie class constructor will call 
+ * The FastZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
@@ -254,7 +312,7 @@
  * -----------------------------
  * Creates a strong zombie.
  *
- * The StrongZombie class constructor will call 
+ * The StrongZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
@@ -277,7 +335,7 @@
  * -----------------------------
  * Creates a ranged zombie.
  *
- * The RangedZombie class constructor will call 
+ * The RangedZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
@@ -300,7 +358,7 @@
  * -----------------------------
  * Creates an exploding zombie.
  *
- * The ExplodingZombie class constructor will call 
+ * The ExplodingZombie class constructor will call
  *   the super class (Zombie) constructor
  *   while passing in the 3 Zombie constructor params
  *
